201f825d0a7767b445fce93f040611ca6c8d7019
NaralaPraveen393 <you@example.com>
2022-02-03 04:36:35 -0800

    Day9 Assignment submission

---
+++ Day9 Morning Assignments/Day9Project3/Day9Project3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9Project3
+{       //**********************************************\\
+        //Author:Narala Praveen
+        //Purpose:To create a employee class and print data
+        //**********************************************\\
+
+    class Employee
+    {
+        public int id;
+        public string name;
+        public int salary;
+        public static string company = "NationsBenefits";
+
+     public void ReadData()
+        {
+            Console.WriteLine("Enter Employee id:");
+            id=Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Enter Employee Name");
+            name=Console.ReadLine();
+            Console.WriteLine("Enter Employee Salary:");
+            salary=Convert.ToInt32(Console.ReadLine());
+
+            
+        }
+        public void PrintData()
+        {
+            Console.WriteLine($"Id :{id},Name:{name},Salary:{salary},Company={company}");
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            emp1.ReadData();
+            emp1.PrintData();
+
+            Employee emp2 = new Employee();
+            emp2.ReadData();
+            emp2.PrintData();
+
+
+            Console.ReadLine();
+        }
+    }
+}
