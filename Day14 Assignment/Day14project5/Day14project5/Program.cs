d88eccfcab1b8871490601234c66fd91a5aad240
NaralaPraveen393 <you@example.com>
2022-02-10 14:35:03 -0800

    1 to 30 number not divisible by 30

---
+++ Day14 Assignment/Day14project5/Day14project5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14project5
+{   //Author:Narala Praveen
+    //Purpose:print numbers which are not divisble by 3 in the range of 1 to 30
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i=1;i<30;i++)
+            {
+                if (i % 3 == 0)
+                    continue;
+                Console.WriteLine(i);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
