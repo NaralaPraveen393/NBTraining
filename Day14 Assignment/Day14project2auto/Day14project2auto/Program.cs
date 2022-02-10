451db14ec77468e075ecda2692ca5114b9981c25
NaralaPraveen393 <you@example.com>
2022-02-10 14:31:37 -0800

    Auto-implemented properties

---
+++ Day14 Assignment/Day14project2auto/Day14project2auto/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14project2auto
+{      //**********************************************************************\\
+       //Author:Narala Praveen
+       //Purpose:code for Auto-implemented Properties.
+
+    class kineticenergy
+    {
+        
+        private int mass;
+        private int velocity;
+
+        //Normal Properties.
+        public int Mass
+        {
+
+            set { mass = value; }
+        }
+        public int Velocity
+        {
+
+            set { velocity = value; }
+
+        }
+        //Auto-implemented properties:
+        public int Energy
+        {
+            get
+            {
+                return  (mass * velocity * velocity) / 2;
+                
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            kineticenergy k = new kineticenergy();
+            k.Mass = 20;
+            k.Velocity = 12;
+            Console.WriteLine($"Auto implemented Kinetic energy: {k.Energy}");
+            Console.ReadLine();
+        }
+    }
+}
