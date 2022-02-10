81427f8c7e5c6ebd1cf5d6aac14ef3e872b8f25e
NaralaPraveen393 <you@example.com>
2022-02-10 14:33:15 -0800

    Prime number using break

---
+++ Day14 Assignment/Day14projec4/Day14projec4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14projec4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int i;
+            
+            
+            Console.WriteLine("Enter any number");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for ( i = 2; i <= n; i++)
+            {
+                if (n % i == 0)
+                    break;
+            }
+                if (i == n)
+                    Console.WriteLine("The number is prime");
+                else
+                    Console.WriteLine("the number is not prime");
+            
+            Console.ReadLine();
+                
+
+            
+        }
+    }
+}
