6f69c3ee9e7e91b59412a78c715441397465b2bf
NaralaPraveen393 <you@example.com>
2022-02-10 14:36:37 -0800

    Number above 1000 divided by 97

---
+++ Day14 Assignment/Day14project6/Day14project6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14project6
+{     //*********************************************************\\
+     //Author:Narala Praveen
+     //Purpose:print the number which is divisible by 97 after 1000
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int n = 97;
+            for(i=1000;i<1097;i++)
+            {
+                if (i % n== 0)
+                    break;
+            }
+            Console.WriteLine("The number divisible by 97 is {0}",i);
+            Console.ReadLine();
+        }
+    }
+}
