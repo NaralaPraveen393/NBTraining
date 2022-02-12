ea972319d5a7bd5fd97256b75c92968ed372a712
NaralaPraveen393 <you@example.com>
2022-02-12 09:38:54 -0800

    Streamreader

---
+++ Day15 Assignment/Day15project3/Day15project3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15project3
+{
+    internal class Program
+    {
+        public static object Day15project1 { get; private set; }
+
+        static void Main(string[] args)
+        {
+            StreamWriter sw = new StreamWriter("C:\\NBHtraining\\Day14 Assignment\\Day15project1\\folder1\\Praveen.txt",true);
+            sw.WriteLine("Hi");
+            sw.WriteLine("These is praveen");
+            sw.WriteLine("File has created");
+            sw.Close();
+            Console.WriteLine("File done");
+            Console.ReadLine();
+
+        }
+    }
+}
