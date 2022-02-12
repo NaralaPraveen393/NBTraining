135aa5d7daffbda3dbce69a539ecda62f305f093
NaralaPraveen393 <you@example.com>
2022-02-12 09:33:05 -0800

    Read data from file

---
+++ Day15 Assignment/Day15Project4/Day15Project4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day15Project4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(File.ReadAllText("C:\\NBHtraining\\Day14 Assignment\\Day15project1\\folder1\\Praveen.txt"));
+            Console.ReadLine();
+        }
+    }
+}
