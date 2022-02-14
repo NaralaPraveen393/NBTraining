cbfe1acb52caa610d18d15f2f8b279013884bd51
NaralaPraveen393 <you@example.com>
2022-02-14 09:35:14 -0800

    Praveen Library

---
+++ Day16 Assignment/PraveenLibrary/PraveenLibrary/Mathematics.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraveenLibrary
+{
+    public class Mathematics
+    {
+        public static int GetFactorial()
+        {
+            int input;
+            int fact = 1;
+            Console.WriteLine("Enter number");
+            input=Convert.ToInt32(Console.ReadLine());
+            for(int i =1;i<=input;i++)  
+                fact=fact*i;
+            return fact;
+        }
+        public static int Add(int a,int b)
+        {
+            return a+b;
+        }
+        public static int Multiplication(int a,int b)
+        {
+            return a * b;
+        }
+        public static int Division(int a, int b)
+        {
+            return a / b;
+        }
+
+    }
+}
---
+++ Day16 Assignment/PraveenLibrary/PraveenLibrary/chemistry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraveenLibrary
+{
+    public  class chemistry
+    {
+        public string GetBenzene()
+        {
+            return "C6H6";
+        }
+        public string GetWater()
+        {
+            return "H20";
+        }
+        public string GetMethane()
+        {
+            return "CH4";
+        }
+    }
+}
