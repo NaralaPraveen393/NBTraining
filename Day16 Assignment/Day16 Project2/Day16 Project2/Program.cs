40a7df9f0bd842b6e4970005f4677fa00388d1ce
NaralaPraveen393 <you@example.com>
2022-02-14 09:26:47 -0800

    Factorial

---
+++ Day16 Assignment/Day16 Project2/Day16 Project2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16_Project2
+{  //Author:Narala Praveen
+    //Purpose:Code for Factorial
+
+    class Maths
+    {
+        public static int GetFactorial()
+        {
+            int input; int fact = 1;
+            Console.WriteLine("Enter input");
+            input = Convert.ToInt32(Console.ReadLine());
+            
+            
+            for(int i =1;i<=input;i++)
+                fact=fact*i;    
+                return fact;
+         
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(Maths.GetFactorial());
+            Console.ReadLine();
+        }
+    }
+}
