a7f6620c757554729405626262b821bac36408cb
NaralaPraveen393 <you@example.com>
2022-02-14 09:24:49 -0800

    Hello world

---
+++ Day16 Assignment/Day16 Project1/Day16 Project1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16_Project1
+{     //Author :Narala Praveen
+      //Purpose:Code for hello program.
+    class Hello
+    {
+        public static void PrintHello()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+    internal class Program
+    { 
+        static void Main(string[] args)
+        {
+           Hello.PrintHello();
+            Console.ReadLine();
+        }
+    }
+}
