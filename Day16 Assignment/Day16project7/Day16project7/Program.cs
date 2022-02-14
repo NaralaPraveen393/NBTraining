79e2cd47f2a9f593b771c9f4fc5fd21f554ab017
NaralaPraveen393 <you@example.com>
2022-02-14 09:32:18 -0800

    Pallindrome

---
+++ Day16 Assignment/Day16project7/Day16project7/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16project7
+{  //Author:Narala Praveen
+    //Purpose:Code for Pallidrome
+
+    class Pallindrome
+    {
+        public int n;
+        public int rem;
+        public int rev;
+        public int temp;
+
+        public void ReadData()
+        {
+           
+            Console.WriteLine("Enter number");
+            n=Convert.ToInt32(Console.ReadLine());   
+        }
+        public void GetPallindrome()
+        {
+            temp = n;
+            while(n >0)
+            {
+                rem = n % 10;
+                rev = (rev * 10) + rem;
+                
+                n = n / 10;
+            }
+            if (temp==rev)
+            
+                Console.WriteLine("The number  is pallindrome");
+            
+            else
+
+                Console.WriteLine("The number is not Pallindrome");
+            
+            
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pallindrome p=new Pallindrome();
+            p.ReadData();
+            p.GetPallindrome();
+            Console.ReadLine();
+
+        }
+    }
+}
