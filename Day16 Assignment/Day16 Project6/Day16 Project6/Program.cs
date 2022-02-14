7c4188629d2a3b014a8569848c7c908e699f543b
NaralaPraveen393 <you@example.com>
2022-02-14 09:30:59 -0800

    Multiplication

---
+++ Day16 Assignment/Day16 Project6/Day16 Project6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16_Project6
+{ //Author :Narala Praveen
+  //Purpose:Code for Multiplication table for a given number.
+
+    class Multiplication
+    {
+        
+        
+            int n;
+            public void ReadData()
+            {
+                Console.WriteLine("Enter number");
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+        public void GetMultiplication()
+        { 
+            for (int i = 1; i <= 10; i++)
+            {
+                
+                Console.WriteLine(n+"x"+i+"="+n*i);
+                
+            }
+           
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Multiplication m = new Multiplication();
+            m.ReadData();
+             m.GetMultiplication();
+            Console.ReadLine();
+        }
+    }
+}
