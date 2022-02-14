ad0d2be32ec089755b5f2131e339834ddb863d64
NaralaPraveen393 <you@example.com>
2022-02-14 09:29:22 -0800

    libray

---
+++ Day16 Assignment/Day16project5/Day16project5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PraveenLibrary;
+
+namespace Day16project5
+{  //Author:Narala Praveen
+    //Purpose:To implement Library methods.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //chemistry c = new chemistry();
+
+            //Console.WriteLine(c.GetBenzene());
+            //Console.WriteLine(Mathematics.Add(2,5));
+            Console.WriteLine(physics.FinalVelocity(5, 2, 5));
+            Console.ReadLine();
+        }
+    }
+}
