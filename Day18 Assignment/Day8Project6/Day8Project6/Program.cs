5dc9f0d6dc9079a79a5bde51bdbe086088d987d7
NaralaPraveen393 <you@example.com>
2022-02-16 01:52:31 -0800

    6,7,8 projects

---
+++ Day18 Assignment/Day8Project6/Day8Project6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Maths;
+
+namespace Day8Project6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"The factorial of 4 {Algebra.Factorial(4)}");
+            Console.WriteLine($"The factorial of 0 is {Algebra.Factorial(0)}");
+            Console.WriteLine($"The factorial of 8 is {Algebra.Factorial(8)}");
+            Console.WriteLine($"The factorial of -1 is{Algebra.Factorial(-1)}");
+            Console.WriteLine(Algebra.Palindrome(363));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Day18 Assignment/Day8Project6/MathsTests/AlgebraTests.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Maths;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maths.Tests
+{
+    [TestClass()]
+    public class AlgebraTests
+    {
+        [TestMethod()]
+        public void FactorialTest_Zero_Input()
+        {
+            //Arrange
+            int n = 0;
+            int expected = 1;
+
+            //Act
+            int actual=Algebra.Factorial(n);    
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void Factorialtest_Greaterthan_seven()
+        {
+            //Arrange
+            int n = 8;
+            int expected = -999;
+
+            //Act
+            int actual = Algebra.Factorial(n);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod()]
+        public void Factorialtest_Lessthan_Zero()
+        {
+            //Arrange
+            int n = -2;
+            int expected = -9999;
+            
+            //Act
+            int actual=Algebra.Factorial(n);
+            
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void Factorialtest_Lessthan_Seven()
+        {
+            //Arrange
+            int n = 5;
+            int expected = 120;
+
+            //Act
+            int actual = Algebra.Factorial(n);  
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod()]
+        public void Palindrometest()
+        {
+            //Arrange
+            int n = 363;
+            string expected = "Palindrome";
+
+            //Act
+            string actual=Algebra.Palindrome(n);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod()]
+        public void Not_Palindrometest()
+        {
+            //Arrange
+            int n = 123;
+            string expected = "Not Palindrome";
+
+            //Act
+            string actual = Algebra.Palindrome(n);
+
+            //Assert
+            Assert.AreEqual(expected,actual);   
+        }
+
+    }
+}
