d13d44797daf6bd0837ca10ecb6240b4da269372
NaralaPraveen393 <you@example.com>
2022-02-17 07:07:48 -0800

    XML file in C#

---
+++ Day19Assignment/Day19project1/Day19project1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Day19project1
+{
+    internal class Program
+    {  //Author:Narala Praveen
+        //Purpose:To read XML file in C#
+        static void Main(string[] args)
+        {
+            
+            XmlDocument doc = new XmlDocument();
+            doc.Load("C:\\NBHtraining\\XML files\\Tag base XML.xml");
+
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                string text = node.InnerText;
+                Console.WriteLine(text);
+            } 
+            Console.ReadLine(); 
+        }  
+    }
+}
