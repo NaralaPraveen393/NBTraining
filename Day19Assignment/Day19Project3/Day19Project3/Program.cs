3c533e831747ff821763364c6464a745943c3a51
NaralaPraveen393 <you@example.com>
2022-02-17 07:14:30 -0800

    XML code with ,

---
+++ Day19Assignment/Day19Project3/Day19Project3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Day19Project3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("C:\\NBHtraining\\XML files\\Tag base XML.xml");
+
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                foreach (XmlNode nodes in node.ChildNodes)
+                {
+                    if (nodes.Name == "ID")
+                    {
+                        Console.Write(nodes.InnerText+", ");
+                    }
+                    if (nodes.Name == "Name")
+
+                    {
+                        Console.Write(nodes.InnerText+", ");
+                    }
+                    if (nodes.Name == "Price")
+                    {
+                        Console.Write($"{nodes.InnerText}\n");
+                    }
+                }
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
