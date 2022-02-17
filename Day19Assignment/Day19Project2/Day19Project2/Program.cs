1997008bfb238361d9fd5a9412bf93be446729a2
NaralaPraveen393 <you@example.com>
2022-02-17 07:12:47 -0800

    XML code only name

---
+++ Day19Assignment/Day19Project2/Day19Project2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Day19Project2
+{   //Author:Narala Praveen
+    //Purpose:To print only name from XML file
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load("C:\\NBHtraining\\XML files\\Tag base XML.xml");
+
+            foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+            {
+                foreach (XmlNode node2 in node.ChildNodes)
+                {
+                    if (node2.Name == "Name")
+
+                        Console.WriteLine(node2.InnerText);
+                }
+               
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
