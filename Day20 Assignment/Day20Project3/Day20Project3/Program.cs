175c9814a8a0935ae7935903a2b9df39a88d8a4b
NaralaPraveen393 <you@example.com>
2022-02-19 08:22:19 -0800

    Nullable types

---
+++ Day20 Assignment/Day20Project3/Day20Project3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20Project3
+{  //Author:Narala Praveen
+    //Purpose:To illustrate nullable types
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Nullable <int> n = 20;
+
+            if(n.HasValue)
+
+            Console.WriteLine(n*n);
+            else
+                Console.WriteLine("no value");  
+            Console.ReadLine();
+        }
+    }
+}
