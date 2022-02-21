7fe2a94582eef489fa849f7da7e46d86d64444a8
NaralaPraveen393 <you@example.com>
2022-02-20 23:38:17 -0800

    Consuming of Services in Console and windows app

---
+++ Day21 Assignment/MyClintApp/MyClintApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using MyClintApp.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClintApp
+{ //Author:Narala Praveem
+    //Purpose:To check the Web services methods 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PraveenNaralaserviceSoapClient pc = new PraveenNaralaserviceSoapClient();
+            Console.WriteLine($"Factorial is{pc.Factorial(4)}");
+            Console.WriteLine($"Addition is {pc.Add(6,8)}");
+            Console.WriteLine($"Multiplication is {pc.Multiplication(5,6)}");
+            Console.WriteLine($"Division is {pc.Division(8,6)}");
+            Console.WriteLine($"Subtraction is {pc.Subtract(21,15)}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ Day21 Assignment/MyClintApp/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1.ServiceReference1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {  //Author:Narala Praveen
+        //Purpose: To consume web services in windows app.
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = Convert.ToInt32(textBox1.Text);
+            PraveenNaralaserviceSoapClient pc = new PraveenNaralaserviceSoapClient();
+            textBox2.Text = pc.Factorial(n).ToString();
+        }
+    }
+}
